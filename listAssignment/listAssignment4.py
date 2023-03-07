564a61262519e3bfaa0b5174b4b8fcf478197150
Gurjas Singh <gsingh.ont@gmail.com>
2023-03-07 09:12:12 -0500

    done list assignment q's

--- listAssignment/listAssignment4.py
+++ listAssignment/listAssignment4.py
@@ -1,3 +1,16 @@
+import sys
+
 n = int(input("How many people are there in the game? "))
 graph = [int(i) for i in input("Enter the names space seperately: ").split()]
-print(graph)
+visited = []
+prev = graph[0]
+
+for i in range(n):
+    if prev in visited:
+        print('Invalid')
+        sys.exit()
+
+    visited.append(prev)
+    prev = graph[prev]
+
+print('Valid')
