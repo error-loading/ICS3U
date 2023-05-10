b82021bc0a8dc39d2e843b16db1923eed00c09c8
Gurjas Singh <gsingh.ont@gmail.com>
2023-05-09 23:35:01 -0400

    finished comments on function assignment

--- functionAssignment/funcAssign3.py
+++ functionAssignment/funcAssign3.py
@@ -1,24 +1,39 @@
+'''
+Gurjas Dhillon
+funcAssign3.py
+create a justify function
+'''
+
 def justify(list1, sz):
     n = len(list1)
 
+    # base cases, if the field size is too small, or nothing was inputed
     if 2*n - 1 > sz or list1 == [] or len(list1) == 1:
         return " ".join(list1)
     
+    # code to get number of chars in the list
     char = 0
 
     for word in list1:
         char += len(word)
     
+    # avg spaces between letters
     avg = (sz - char) // (len(list1) - 1)
 
+    # how many remaining spaces are left over
     rem = sz - char - avg * (len(list1) - 1)
 
     sent = ""
 
     for i in range(len(list1)):
+        # add the word
         sent += list1[i]
-        sent += " " * avg
 
+        # add the avg spaces as long as it's not the last word
+        if i != n - 1:
+            sent += " " * avg
+
+        # add one space at the end if remaining is greater than zero
         if rem > 0:
             sent += " "
             rem -= 1
@@ -26,30 +41,4 @@ def justify(list1, sz):
 
     return sent
 
-
-story = [
-"Vincent Massey Secondary School has again",
-"received top marks from the Fraser Institute.",
-"The national think-tank, which provides annual",
-"test scores for schools across Ontario,",
-"released its findings this month.",
-"",
-"Most schools in Windsor-Essex showed improvement,",
-"but Massey scored highest locally with 8.2 out of",
-"10 to finish 50th out of 740 schools in the province.",
-"",
-"St. Anne Catholic High School in Lakeshore was the",
-"second place local finisher for the 2015-2016 year",
-"with a score of 8.1 and earning 57th place.",
-""
-"Schools are scored based on seven academic indicators",
-"found in the results of annual math and literacy",
-"tests.",
-"",
-"When the institute released its report on elementary",
-"schools in December, Bellewood Public School scored",
-"top marks for the area with a 9.3 — good enough for",
-"35th place out of 2,900 schools across the province."]
-
-for line in story:
-    print("|",justify(line.split(),56),"|")
+print("|", justify(["all", "too", "easy"], 15), "|")
